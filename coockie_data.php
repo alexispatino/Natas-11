17611973d35c585acbe086e26eeca9241cfe4389
alexispatino <111712163+alexispatino@users.noreply.github.com>
2024-05-02 23:31:59 -0600

    Create coockie_data.php

---
+++ coockie_data.php
@@ -0,0 +1,20 @@
+//Reutilizando la función xor_encrypt del código original, 
+ podemos pasarle la cookie data e indicando que la variable $key será $defaultdata (codificada en json)//
+
+$defaultdata = array( "showpassword"=>"no", "bgcolor"=>"#ffffff");
+$cookie = "ClVLIh4ASCsCBE8lAxMacFMZV2hdVVotEhhUJQNVAmhSEV4sFxFeaAw=";
+
+function xor_encrypt($in, $defaultdata) {
+    $key = $defaultdata;
+    $text = $in;
+    $outText = '';
+  
+    // Iterate through each character
+    for($i=0;$i<strlen($text);$i++) {
+        $outText .= $text[$i] ^ $key[$i % strlen($key)];
+    }
+
+    return $outText;
+}
+
+echo xor_encrypt(base64_decode($cookie), json_encode($defaultdata));
